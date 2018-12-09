9f1cccad26859195d8a300a85148308adacedf21
enhoker <45732134+enhoker@users.noreply.github.com>
2018-12-09 19:51:56 +0300

    Добавлена 4 задача

---
+++ Task4.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "Tack1_3"
+
+int main(void) {
+	char c = '0';
+	int x;
+	struct list l = list_new();
+	while (c != '\n') {
+		scanf_s("%c", &c);
+		if (c == '(') list_push(&l, 0);
+		if (c == '{') list_push(&l, 1);
+		if (c == ')') {
+			list_pop(&l, &x);
+			if (x != 0) {
+				printf("NO");
+				return 0;
+			}
+		}
+		if (c == '}') {
+			list_pop(&l, &x);
+			if (x != 1) {
+				printf("NO");
+				return 0;
+			}
+		}
+	} 
+	if (l.beg != NULL) printf("NO");
+	else printf("YES");
+}
