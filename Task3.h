f0387e35dad8e840a2218dca50d1aa20b61a658f
fireocean <fairushinaidar2016D@yandex.ru>
2018-12-16 22:53:52 +0300

    Добавил комментарии

--- Task3.h
+++ Task3.h
@@ -1,24 +1,30 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-
+//Структура "ячейка" - целое число +
+//2 ссылки на пред. и след. элементы
 typedef struct node {
 	int value;
 	struct node* prev;
 	struct node* next;
 } node;
-
+//Структура дв. списка - 2 ссылки на
+//начало и конец списка.
 typedef struct list {
 	node* beg;
 	node* fin;
 } list;
-
+//Функция создания пустого списка
+//Выделение памяти для двух ссылок
 list* list_new(void) {
-	list* a = malloc(sizeof(int) + 2 * sizeof(void*));
+	list* a = malloc(2 * sizeof(void*));
 	a->beg = NULL;
 	a->fin = NULL;
 	return a;
 }
-
+//Удаление списка. Последовательно
+//освобождается память для всех ячеек
+//а затем освобождается память, в которой
+//хранились ссылки на начальный и конечный элементы списка
 int list_delete(list* l) {
 	node* a = l->beg;
 	node* b;
@@ -33,7 +39,10 @@ int list_delete(list* l) {
 	free(l);
 	return 0;
 }
-
+//Добавление элемента в конец
+//Занимаем память для него и "привязываем"
+//его к концу списка. Если список пуст
+//делаем новый элемент началом и концом этого списка
 int list_push(list* l, int a) {
 	if (l == NULL) {
 		return 1;
@@ -51,7 +60,11 @@ int list_push(list* l, int a) {
 	l->fin = q;
 	return 0;
 }
-
+//Удаляем элемент с конца.
+//Если список пуст, ничег не делаем,
+//вернется исключение.
+//Если в списке один элемент,
+//Делаем список пустым
 int list_pop(list* l, int* x) {
 	if (l == NULL) {
 		exit(1);
@@ -75,7 +88,7 @@ int list_pop(list* l, int* x) {
 		};
 	};
 }
-
+//Аналогично Push, но в начале
 int list_unshift(list* l, int a) {
 	if (l == NULL) {
 		return 1;
@@ -93,7 +106,7 @@ int list_unshift(list* l, int a) {
 	l->beg = q;
 	return 0;
 }
-
+//Аналогично Pop, но в начале
 int list_shift(list* l, int* x) {
 	if (l == NULL) {
 		exit(1);
