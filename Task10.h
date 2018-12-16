e2dfd0d9dd87428591abb7b03befc9fe4cb1ee0c
enhoker <45732134+enhoker@users.noreply.github.com>
2018-12-17 02:01:30 +0300

    Добавила функцию insert

--- Task10.h
+++ Task10.h
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 struct Record {
 	char* name;
 	long long int number;
 };
 
 struct Notebook {
-	struct Record 
+	struct Record * book;
+	int busy;
+	int site;
 };
+
+void insert(struct Notebook * N, char* name, long long int number) {
+	if (N->site - N->busy < 1) {
+		struct Record * tmp = malloc(sizeof(struct Record) * N->site * 2);
+		memcpy(tmp, N->book, sizeof(struct Record) * N->busy);
+		free(N->book);
+		N->book = tmp;
+		N->site = 2 * N->site;
+	}
+	N->book[N->busy].name = name;
+	N->book[N->busy].number = number;
+	N->busy++;
+}
