1302a56b82f7e2bd98cc2515a7f46b0bdf1df2c4
enhoker <45732134+enhoker@users.noreply.github.com>
2018-12-16 16:20:26 +0300

    Update Task4.c

--- Task4.c
+++ Task4.c
@@ -5,26 +5,28 @@
 int main(void) {
 	char c = '0';
 	int x;
-	struct list l = list_new();
+	list* l = list_new();
 	while (c != '\n') {
-		scanf("%c", &c);
-		if (c == '(') list_push(&l, 0);
-		if (c == '{') list_push(&l, 1);
+		scanf_s("%c", &c);
+		if (c == '(') push(l, 0);
+		if (c == '{') push(l, 1);
 		if (c == ')') {
-			list_pop(&l, &x);
+			pop(l, &x);
 			if (x != 0) {
 				printf("NO");
 				return 0;
 			}
 		}
 		if (c == '}') {
-			list_pop(&l, &x);
+			pop(l, &x);
 			if (x != 1) {
 				printf("NO");
 				return 0;
 			}
 		}
-	} 
-	if (l.beg != NULL) printf("NO");
+	}
+	if (l->head != NULL) printf("NO");
 	else printf("YES");
+
+	return 0;
 }
