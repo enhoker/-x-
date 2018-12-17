34a277d9aa4e5f1cf2e62fd06d23717a2b1b31c2
enhoker <45732134+enhoker@users.noreply.github.com>
2018-12-17 03:12:43 +0300

    Добавила поиск

--- Task10.h
+++ Task10.h
@@ -14,7 +14,28 @@ struct Notebook {
 	int site;
 };
 
-void insert(struct Notebook * N, char* name, long long int number) {
+int find(struct Notebook * N, char * name) {
+	int i = 0;
+	for (i = 0; i < N->busy; i++) {
+		if (N->book[i].name == name) {
+			printf("number %lld", N->book[i].number);
+			return 0;
+		}
+	}
+	printf("NO");
+	return 1;
+}
+
+// Добавление записи
+int insert(struct Notebook * N, char* name, long long int number) {
+	int i = 0;
+	for (i = 0; i < N->busy; i++) {
+		if (N->book[i].name == name) {
+			printf("Changed. Old value = %ld", N->book[i].number);
+			N->book[i].number = number;
+			return 1;
+		}
+	}
 	if (N->site - N->busy < 1) {
 		struct Record * tmp = malloc(sizeof(struct Record) * N->site * 2);
 		memcpy(tmp, N->book, sizeof(struct Record) * N->busy);
@@ -25,4 +46,5 @@ void insert(struct Notebook * N, char* name, long long int number) {
 	N->book[N->busy].name = name;
 	N->book[N->busy].number = number;
 	N->busy++;
+	return 0;
 }
