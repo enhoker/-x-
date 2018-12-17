8e79328c2bd6f2bd21436b2ed9328fb3bcfd66e6
enhoker <45732134+enhoker@users.noreply.github.com>
2018-12-17 04:00:44 +0300

    Ура! Я добавиль 13 задачу!
    Не знаю, сделала ли я именно то, что нужно, но время 4 утра и мне лень задумываться над такими сложными материями

---
+++ Task13.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Task8.h"
+
+// Функция check_tree проверяет, является ли поддерево вершины tree АВЛ-деревом
+//Функция принимает указатель на вершину, поддерево которой мы хотим проверить, 
+// и указатель на переменную, в которую мы будем записывать глубину поддерева
+// Функция возвращает 1, если поддерево является АВЛ-деревом и 0 в противном случае
+int check_tree(struct Point * tree, int * depth) {
+	int check, left, right;
+	check = check_tree(tree->left, &left) * check_tree(tree->right, &right);
+	*depth = max(left, right) + 1;
+	return check;
+}
+
